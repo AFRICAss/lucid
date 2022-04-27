8e3e4f296af253077bf480fb91a5b303e48c9e67
pyro <90619153+pyroTM@users.noreply.github.com>
2022-04-27 13:38:44 -0500

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: 'https://cdn.nyxevade.gq/bare/',
+    bare: 'https://cdn.peardeck.cf/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
